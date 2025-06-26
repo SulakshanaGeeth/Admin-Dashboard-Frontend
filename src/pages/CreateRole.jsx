42440972af58b3d4022b277d3c27a4530daff6f5
SulaskshnaGeeth <sulaskshanageeth@gmail.com>
2025-06-26 21:47:58 +0530

    minor change on the create role page

--- src/pages/CreateRole.jsx
+++ src/pages/CreateRole.jsx
@@ -1,6 +1,30 @@
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const CreateRole = () => {
+    const [roleName, setRoleName] = useState('');
+    const handleRoleNameChange = (e) => {
+        setRoleName(e.target.value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!roleName.trim()) {
+            alert('Role name cannot be empty!');
+            return;
+        }
+    }
+
     return (
-        <h1 > Create Role</h1>
+        <Form>
+            <Form.Group className="mb-3" onSubmit={handleSubmit}>
+                <Form.Label>Role Name</Form.Label>
+                <Form.Control type="string" placeholder="role name" onChange={handleRoleNameChange} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Create Role
+            </Button>
+        </Form>
     )
 }
 export default CreateRole;
