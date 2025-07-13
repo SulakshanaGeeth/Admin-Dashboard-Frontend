dac15df8f335230dbf9eb6b34e28233272a1426f
SulaskshnaGeeth <sulaskshanageeth@gmail.com>
2025-07-13 19:17:30 +0530

    minor bug fix on incorrect imports

--- src/pages/CreateRole.jsx
+++ src/pages/CreateRole.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { createRole } from '../services/userService';
+import { createRole } from '../services/roleService';
 
 const CreateRole = () => {
     const [roleName, setRoleName] = useState('');
