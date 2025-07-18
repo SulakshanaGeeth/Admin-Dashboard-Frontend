9409e4fcebc18268587cc85fdb747ab0912c9449
SulaskshnaGeeth <sulaskshanageeth@gmail.com>
2025-07-18 21:29:47 +0530

    View Permissions page implemeted

--- src/pages/ViewPermissions.jsx
+++ src/pages/ViewPermissions.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
+import { getPermissions } from '../services/permissionService';
+import { Table, Modal, Button, Spinner } from 'react-bootstrap';
+
 const ViewPermissions = () => {
+    const [permissions, setPermissions] = useState([]);
+    useEffect(() => {
+        fetchPermissions();
+    }, []);
+    const fetchPermissions = async () => {
+        try {
+            const response = await getPermissions();
+            setPermissions(response);
+        } catch (error) {
+            console.error('Error fetching permissions:', error);
+        }
+    }
     return (
         <div>
             <h1>View Permission</h1>
-            <p>This page is under construction.</p>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th >ID</th>
+                        <th >Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {permissions.map(permission => (
+                        <tr key={permission.id}>
+                            <td>{permission.id}</td>
+                            <td >{permission.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </div>
     );
 }
