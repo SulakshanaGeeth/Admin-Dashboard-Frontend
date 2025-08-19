ca32f066fd24a19fbaa528fd7c0046bfddfe181e
SulaskshnaGeeth <sulaskshanageeth@gmail.com>
2025-08-19 21:45:29 +0530

    change hashRouter to BrowserRouter

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./pages/Dashboard'))
+const CreateUser = React.lazy(() => import('./pages/CreateUser'))
+const CreateRole = React.lazy(() => import('./pages/CreateRole'))
+const ViewRoles = React.lazy(() => import('./pages/ViewRoles'))
+const ViewPermissions = React.lazy(() => import('./pages/ViewPermissions'))
+const ViewUsers = React.lazy(() => import('./pages/ViewUsers'))
 
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/create-user', name: 'CreateUser', element: CreateUser },
+  { path: '/create-role', name: 'CreateRole', element: CreateRole },
+  { path: '/view-roles', name: 'ViewRole', element: ViewRoles },
+  { path: '/view-permissions', name: 'ViewPermissions', element: ViewPermissions },
+  { path: '/view-users', name: 'ViewUsers', element: ViewUsers }
 ]
 
 export default routes
